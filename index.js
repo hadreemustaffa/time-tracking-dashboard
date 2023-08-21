7cd3f81df1a2dcff162eeecf88ebee115d3c5b00
drevkr <hadree.mustaffa@gmail.com>
2023-08-22 05:10:05 +0800

    feat: finish render cards using JS function

--- index.js
+++ index.js
@@ -1,46 +1,89 @@
-const activityName = document.querySelectorAll('.activity-name');
-const current = document.querySelectorAll('.current');
-const previous = document.querySelectorAll('.previous');
-const selectedPeriod = document.querySelectorAll('.selected-period');
-const timePeriods = document.querySelectorAll('.time-period');
-
+// fetch data from data.json
 async function fetchData() {
-	const response = await fetch('./data.json');
-	const data = await response.json();
-	console.log(data);
+	try {
+		const response = await fetch('./data.json');
+		return (data = await response.json());
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-	for (let i = 0; i < data.length; i++) {
-		activityName[i].innerHTML = data[i].title;
+// render cards using data return from json
+async function renderCards() {
+	let allData = await fetchData();
+	let cardSection = '';
+
+	allData.forEach((data) => {
+		let card = `<div class="card activity">
+      <div class="card-top activity ${data.title.toLowerCase()}">
+        <img src=${data.icon} alt="">
+      </div>
+      <div class="card-bottom activity">
+        <div class="activity-detail">
+          <p class="activity-name" class="text-neutral-5">${data.title}</p>
+          <button aria-label="edit" class="ellipsis">
+            <img src="images/icon-ellipsis.svg" alt="">
+          </button>
+        </div>
+        <div class="time-spent">
+          <h1 class="text-neutral-5"><span class="current"></span>hrs</h1>
+          <p><span class="selected-timeframe fs-secondary-body"></span> - <span class="previous"></span>hrs</p>
+        </div>
+      </div>
+    </div>`;
+
+		cardSection += card;
+	});
+
+	let cardsContainer = document.querySelector('.cards-container');
+	cardsContainer.innerHTML = cardSection;
+
+	// set data based on selected timeframe
+	for (let i = 0; i < allData.length; i++) {
+		const timeFrames = document.querySelectorAll('.timeframe');
+		const current = document.querySelectorAll('.current');
+		const previous = document.querySelectorAll('.previous');
+		const selectedTimeframe = document.querySelectorAll('.selected-timeframe');
 
 		function getDailyData() {
-			current[i].innerHTML = data[i].timeframes.daily.current;
-			previous[i].innerHTML = data[i].timeframes.daily.previous;
-			selectedPeriod[i].innerHTML = 'Yesterday';
+			current[i].innerHTML = allData[i].timeframes.daily.current;
+			previous[i].innerHTML = allData[i].timeframes.daily.previous;
+			selectedTimeframe[i].innerHTML = 'Yesterday';
 		}
 		function getWeeklyData() {
-			previous[i].innerHTML = data[i].timeframes.weekly.previous;
-			current[i].innerHTML = data[i].timeframes.weekly.current;
-			selectedPeriod[i].innerHTML = 'Last Week';
+			current[i].innerHTML = allData[i].timeframes.weekly.current;
+			previous[i].innerHTML = allData[i].timeframes.weekly.previous;
+			selectedTimeframe[i].innerHTML = 'Last Week';
+			timeFrames[1].classList.add('active');
 		}
 		function getMonthlyData() {
-			current[i].innerHTML = data[i].timeframes.monthly.current;
-			previous[i].innerHTML = data[i].timeframes.monthly.previous;
-			selectedPeriod[i].innerHTML = 'Last Month';
+			current[i].innerHTML = allData[i].timeframes.monthly.current;
+			previous[i].innerHTML = allData[i].timeframes.monthly.previous;
+			selectedTimeframe[i].innerHTML = 'Last Month';
 		}
 
-		// default on page load
+		// set default active data on page load
 		getWeeklyData();
 
-		timePeriods[0].addEventListener('click', () => {
+		// add 'active' class to selected timeframe
+		timeFrames.forEach((timeframe) => {
+			timeframe.addEventListener('click', () => {
+				document.querySelector('.active')?.classList.remove('active');
+				timeframe.classList.add('active');
+			});
+		});
+
+		// enable click to select timeframe
+		timeFrames[0].addEventListener('click', () => {
 			getDailyData();
 		});
-		timePeriods[1].addEventListener('click', () => {
+		timeFrames[1].addEventListener('click', () => {
 			getWeeklyData();
 		});
-		timePeriods[2].addEventListener('click', () => {
+		timeFrames[2].addEventListener('click', () => {
 			getMonthlyData();
 		});
 	}
 }
 
-fetchData();
+renderCards();
